5d75360713d780e20d5107df3ad86c8acd6d906d
arthyom <a.gonzalezgavina@ugto.mx>
2023-08-10 01:19:17 -0600

    Ciclo de vida de pruebas en nunit, generacion de valores y descripcion de preubas

--- LabNunitMaquinaCafe/MaquinaCafeTestNunit.cs
+++ LabNunitMaquinaCafe/MaquinaCafeTestNunit.cs
@@ -4,61 +4,133 @@ using System;
 
 namespace LabNunitMaquinaCafe
 {
-    public class Tests
+    [SetUpFixture]
+    public class SetUpClass
     {
+        protected MaquinaDeCafe cafeteraVacia;
+        protected MaquinaDeCafe cafeteraLlena;
+
+        public SetUpClass()
+        {
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            TestContext.Progress.WriteLine("One Time SetUp desde la clase setup");
+            cafeteraVacia = new MaquinaDeCafe();
+        }
+
+        [OneTimeTearDown]
+        public void OnetimeTearDown()
+        {
+            TestContext.Progress.WriteLine("One Time Tear Down desde la clase setup");
+        }
+
+        /*
+         * Metodos no permitidos en setupfixture
+        [SetUp]
+        public void SetUp()
+        {
+            TestContext.Progress.WriteLine("Set up ejecutado desde la clase heredada");
+            cafeteraLlena = new MaquinaDeCafe(10, 10, 10, 10, 10);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            TestContext.Progress.WriteLine("Tear down ejecutadose desde la clase heredada");
+            cafeteraLlena = null;
+        }
+        */
+    }
+
+    [TestFixture]
+    public class Tests : SetUpClass
+    {
+
+        //private MaquinaDeCafe cafeteraVacia;
+        //private MaquinaDeCafe cafeteraLlena;
+
+        /// <summary>
+        /// Constructor de la clase que sera ejecutado antes que todo
+        /// </summary>
+        public Tests()
+        {
+          //  Codigo heredado 
+          //  cafeteraVacia = new MaquinaDeCafe();
+        }
+
+        /// <summary>
+        /// Codigo que sera reutilizado antes de cualquier prueba
+        /// </summary>
         [SetUp]
         public void Setup()
         {
+            //  Codigo heredado 
+            cafeteraLlena = new MaquinaDeCafe(3, 3, 3, 3, 3);
+        }
+
+        /// <summary>
+        /// Codigo que sera reutilizado solo una vez
+        /// </summary>
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+
         }
 
         [Test]
         public void Constructor_Vacio_Debe_CrearObjetoOk()
         {
+            //Codigo centralizado
             //Arrange
-            MaquinaDeCafe maquina = null;
+            //MaquinaDeCafe maquina = null;
 
             //Actr
-            maquina = new MaquinaDeCafe();
+            //maquina = new MaquinaDeCafe();
 
             //assert
-            Assert.NotNull(maquina);
-            Assert.Null(maquina.estado);
-            Assert.Null(maquina.mensaje);
-            Assert.AreEqual(maquina.Azucar, 0);
-            Assert.AreEqual(maquina.Cafe, 0);
-            Assert.AreEqual(maquina.VasosGrandes, 0);
-            Assert.AreEqual(maquina.VasosMedianos, 0);
-            Assert.AreEqual(maquina.VasosPequenios, 0);
+            Assert.NotNull(cafeteraVacia);
+            Assert.Null(cafeteraVacia.estado);
+            Assert.Null(cafeteraVacia.mensaje);
+            Assert.AreEqual(cafeteraVacia.Azucar, 0);
+            Assert.AreEqual(cafeteraVacia.Cafe, 0);
+            Assert.AreEqual(cafeteraVacia.VasosGrandes, 0);
+            Assert.AreEqual(cafeteraVacia.VasosMedianos, 0);
+            Assert.AreEqual(cafeteraVacia.VasosPequenios, 0);
         }
 
         [Test]
         public void Constructor_Parametros_Debe_CrearObjestoOk()
         {
+            //Codigo centralizado
             //Arrange
-            MaquinaDeCafe maquina = null;
+            //MaquinaDeCafe maquina = null;
 
             //Actr
-            maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+            //maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
 
             //assert
-            Assert.NotNull(maquina);
-            Assert.NotNull(maquina.mensaje);
-            Assert.NotNull(maquina.estado);
-            Assert.AreEqual(maquina.Azucar, 100);
-            Assert.AreEqual(maquina.Cafe, 1000);
-            Assert.AreEqual(maquina.VasosGrandes, 10);
-            Assert.AreEqual(maquina.VasosMedianos, 10);
-            Assert.AreEqual(maquina.VasosPequenios, 10);
+            Assert.NotNull(cafeteraLlena);
+            Assert.NotNull(cafeteraLlena.mensaje);
+            Assert.NotNull(cafeteraLlena.estado);
+            Assert.AreEqual(cafeteraLlena.Azucar, 3);
+            Assert.AreEqual(cafeteraLlena.Cafe, 3);
+            Assert.AreEqual(cafeteraLlena.VasosGrandes, 3);
+            Assert.AreEqual(cafeteraLlena.VasosMedianos, 3);
+            Assert.AreEqual(cafeteraLlena.VasosPequenios, 3);
         }
 
         [Test]
         public void ServirCafe_PedirCafe_Debe_RegresaTrue()
         {
+            //Codigo centralizado
             //Arrange
-            MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+            //MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
 
             //Act
-            bool response = maquina.servirCafe(10);
+            bool response = cafeteraLlena.servirCafe(3);
 
             //Assert
             Assert.True(response);
@@ -67,11 +139,12 @@ namespace LabNunitMaquinaCafe
         [Test]
         public void ServirAzucar_Seleccionar_Debe_RegresarTrue()
         {
+            //Codigo centralizado
             //Arrange
-            MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+            //MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
 
             //Act
-            bool response = maquina.seleccionarAzucar(10);
+            bool response = cafeteraLlena.seleccionarAzucar(3);
 
             //Assert
             Assert.True(response);
@@ -82,11 +155,28 @@ namespace LabNunitMaquinaCafe
         [TestCase(3)]
         public void SeleccionarVaso_Seleccionar_Debe_RegresarTrue(int tipoVaso)
         {
+            //Codigo centralizado
             //Arrange
-            MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+            //MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
 
             //Act
-            bool response = maquina.seleccionarVaso(tipoVaso);
+            bool response = cafeteraLlena.seleccionarVaso(tipoVaso);
+
+            //Assert
+            Assert.True(response);
+        }
+
+        [Test]
+        [Description("Esta es una descripcion de la prueba")]
+        [TestOf(typeof(MaquinaDeCafe))]
+        public void SeleccionarVaso_Seleccionar_Debe_RegresarTrueConValores([Range(1, 3)] int tipoVaso)
+        {
+            //Codigo centralizado
+            //Arrange
+            //MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+
+            //Act
+            bool response = cafeteraLlena.seleccionarVaso(tipoVaso);
 
             //Assert
             Assert.True(response);
@@ -97,11 +187,12 @@ namespace LabNunitMaquinaCafe
         [TestCase(8)]
         public void SeleccionarVaso_Seleccionar_Debe_RegresarErrorSiNoHayVasos(int tipoVaso)
         {
+            //Codigo centralizado
             //Arrange
-            MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+            //MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
 
             //Act
-            Exception exp = Assert.Throws<Exception>(() => maquina.seleccionarVaso(tipoVaso));
+            Exception exp = Assert.Throws<Exception>(() => cafeteraLlena.seleccionarVaso(tipoVaso));
 
             //Assert
             Assert.That(exp.Message.Length >= 0);
@@ -112,15 +203,37 @@ namespace LabNunitMaquinaCafe
         [TestCase(3)]
         public void SeleccionarVaso_Seleccionar_Debe_RegresarErrorSiNoHaIniciado(int tipoVaso)
         {
+            //Codigo centralizado
             //Arrange
-            MaquinaDeCafe maquina = new MaquinaDeCafe();
+            //MaquinaDeCafe maquina = new MaquinaDeCafe();
 
             //Act
-            Exception exp = Assert.Throws<Exception>(() => maquina.seleccionarVaso(tipoVaso));
+            Exception exp = Assert.Throws<Exception>(() => cafeteraVacia.seleccionarVaso(tipoVaso));
 
             //Assert
             Assert.That(exp.Message.Length >= 0);
         }
+
+        /// <summary>
+        /// Codigo que sera ejecutado una sola vez al final de alguna prueba
+        /// </summary>
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+
+        }
+
+        /// <summary>
+        /// Codigo que sera ejecutado despues de cada prueba
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            cafeteraLlena = null;
+        }
+
+
+
 
     }
 }
