c63dc38a3de127d42050b4f51b1f37fe24bc0c38
arthyom <a.gonzalezgavina@ugto.mx>
2023-08-10 02:47:09 -0600

    Agregando la parte de Xunit

--- LabXunitMaquinaCafe/MaquinaCafeTestXunit.cs
+++ LabXunitMaquinaCafe/MaquinaCafeTestXunit.cs
@@ -4,56 +4,73 @@ using MaquinaCafe;
 
 namespace LabXunitMaquinaCafe
 {
-    public class MaquinaCafeTestXunit
+    public class MaquinaCafeTestXunit:IClassFixture<MaquinaDeCafeFixture> //IDisposable
     {
+        private MaquinaDeCafe maquinaVacia;
+        private MaquinaDeCafe maquinaLlena;
+        private MaquinaDeCafeFixture _maquinaCafeFixture;
+
+        public MaquinaCafeTestXunit(MaquinaDeCafeFixture maquinaDeCafeFixture)
+        {
+
+           // maquinaVacia = new MaquinaDeCafe();
+           // maquinaLlena = new MaquinaDeCafe(100, 100, 100, 100, 100);
+            _maquinaCafeFixture = maquinaDeCafeFixture;
+        }
+
         [Fact]
         public void Constructor_Vacio_Debe_CrearObjetoOk()
         {
             //Arrange
-            MaquinaDeCafe maquina = null;
+            //MaquinaDeCafe maquina = null;
 
             //Actr
-            maquina = new MaquinaDeCafe();
+            //maquinaVacia = new MaquinaDeCafe();
 
             //assert
-            Assert.NotNull(maquina);
-            Assert.Null(maquina.estado);
-            Assert.Null(maquina.mensaje);
-            Assert.InRange(maquina.Azucar, 0, 1);
-            Assert.InRange(maquina.Cafe, 0, 1);
-            Assert.InRange(maquina.VasosGrandes, 0, 1);
-            Assert.InRange(maquina.VasosMedianos, 0, 1);
-            Assert.InRange(maquina.VasosPequenios, 0, 1);
+            //Assert.NotNull(maquinaVacia);
+            //Assert.Null(maquinaVacia.estado);
+            //Assert.Null(maquinaVacia.mensaje);
+            //Assert.InRange(maquinaVacia.Azucar, 0, 1);
+            //Assert.InRange(maquinaVacia.Cafe, 0, 1);
+            //Assert.InRange(maquinaVacia.VasosGrandes, 0, 1);
+            //Assert.InRange(maquinaVacia.VasosMedianos, 0, 1);
+            //Assert.InRange(maquinaVacia.VasosPequenios, 0, 1);
+
+            Assert.InRange(_maquinaCafeFixture.maquinaVacia.VasosGrandes, 0, 1);
         }
 
         [Fact]
         public void Constructor_Parametros_Debe_CrearObjestoOk()
         {
             //Arrange
-            MaquinaDeCafe maquina = null;
+            //MaquinaDeCafe maquina = null;
 
             //Actr
-            maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+            //maquinaLlena = new MaquinaDeCafe(100, 1000, 10, 10, 10);
 
             //assert
-            Assert.NotNull(maquina);
-            Assert.NotNull(maquina.mensaje);
-            Assert.NotNull(maquina.estado);
-            Assert.InRange(maquina.Azucar, 1, double.MaxValue);
-            Assert.InRange(maquina.Cafe, 1, double.MaxValue);
-            Assert.InRange(maquina.VasosGrandes, 1, int.MaxValue);
-            Assert.InRange(maquina.VasosMedianos, 1, int.MaxValue);
-            Assert.InRange(maquina.VasosPequenios, 1, int.MaxValue);
+            //Assert.NotNull(maquinaLlena);
+            //Assert.NotNull(maquinaLlena.mensaje);
+            //Assert.NotNull(maquinaLlena.estado);
+            //Assert.InRange(maquinaLlena.Azucar, 1, double.MaxValue);
+            //Assert.InRange(maquinaLlena.Cafe, 1, double.MaxValue);
+            //Assert.InRange(maquinaLlena.VasosGrandes, 1, int.MaxValue);
+            //Assert.InRange(maquinaLlena.VasosMedianos, 1, int.MaxValue);
+            //Assert.InRange(maquinaLlena.VasosPequenios, 1, int.MaxValue);
+
+            Assert.InRange(_maquinaCafeFixture.maquinaLlena.VasosGrandes, 1, int.MaxValue);
+
         }
 
         [Fact]
         public void ServirCafe_PedirCafe_Debe_RegresaTrue()
         {
             //Arrange
-            MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+            //MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
 
             //Act
-            bool response = maquina.servirCafe(10);
+            bool response = maquinaLlena.servirCafe(10);
 
             //Assert
             Assert.True(response);
@@ -63,10 +80,10 @@ namespace LabXunitMaquinaCafe
         public void ServirAzucar_Seleccionar_Debe_RegresarTrue()
         {
             //Arrange
-            MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+            //MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
 
             //Act
-            bool response = maquina.seleccionarAzucar(10);
+            bool response = maquinaLlena.seleccionarAzucar(10);
 
             //Assert
             Assert.True(response);
@@ -79,14 +96,32 @@ namespace LabXunitMaquinaCafe
         public void SeleccionarVaso_Seleccionar_Debe_RegresarTrue( int tipoVaso )
         {
             //Arrange
-            MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
+            //MaquinaDeCafe maquina = new MaquinaDeCafe(100, 1000, 10, 10, 10);
 
             //Act
-            bool response = maquina.seleccionarVaso(tipoVaso);
+            bool response = maquinaLlena.seleccionarVaso(tipoVaso);
 
             //Assert
             Assert.True(response);
         }
 
+        [Fact]
+        public void ServirCafe_MaquinaVacia_RegresaError()
+        {
+            //Arrange
+            //MaquinaDeCafe maquina = new MaquinaDeCafe();
+
+            //Act
+            Exception response = Assert.Throws<Exception>(() => maquinaVacia.servirCafe(1));
+
+            //Assert
+            Assert.NotNull(response.Message);
+        }
+
+        public void Dispose()
+        {
+            maquinaVacia = null;
+            maquinaLlena = null;
+        }
     }
 }
---
+++ MaquinaCafe/MaquinaDeCafeFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaCafe
+{
+    public class MaquinaDeCafeFixture : IDisposable
+    {
+        public MaquinaDeCafe maquinaVacia;
+        public MaquinaDeCafe maquinaLlena;
+
+        public MaquinaDeCafeFixture()
+        {
+            maquinaLlena = new MaquinaDeCafe(100, 100, 100, 100, 100);
+            maquinaVacia = new MaquinaDeCafe();
+        }
+
+        public void Dispose()
+        {
+            maquinaVacia = null;
+            maquinaLlena = null;
+        }
+    }
+}
